c6b615ec979c8440798f12552f3d244b6b1e486b
Carole Arbogast <arbogast.carole@gmail.com>
2021-08-10 17:55:09 +0300

    feat: create Navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,10 @@
 import Link from "next/link";
+import Navbar from "../src/components/Navbar";
 
 export function Home() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/recipes">Recipes</Link>
-          </li>
-          <li>
-            <Link href="/spots">Mushroom spots</Link>
-          </li>
-          <li>Login/Log out</li>
-        </ul>
-      </nav>
+      <Navbar />
       <br></br>
       <div>Search + Filters</div>
       <div>Mushroom list</div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <NavbarWrapper>
+      <Title>Mushrooms</Title>
+      <Navigation>
+        <NavigationItem>
+          <Link href="/recipes">Recipes</Link>
+        </NavigationItem>
+        <NavigationItem>
+          <Link href="/spots">Mushroom spots</Link>
+        </NavigationItem>
+        <NavigationItem>Login/Log out</NavigationItem>
+      </Navigation>
+    </NavbarWrapper>
+  );
+}
+
+const Title = styled.h1`
+  color: grey;
+`;
+
+const Navigation = styled.ul`
+  display: flex;
+  list-style-type: none;
+`;
+
+const NavigationItem = styled.li`
+  margin: 0 0.5rem;
+`;
+
+const NavbarWrapper = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background: lightgreen;
+`;
+
+export default Navbar;
