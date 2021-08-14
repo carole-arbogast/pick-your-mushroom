e4928b64377075a12f6279b030294189aa56b381
Carole Arbogast <arbogast.carole@gmail.com>
2021-08-14 15:46:29 +0300

    refactor: move navbar to _app.tsx to display it on all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from "next/app";
+import Navbar from "../src/components/Navbar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import MushroomList from "../src/components/MushroomList";
-import Navbar from "../src/components/Navbar";
 
 export function Home() {
   return (
     <>
-      <Navbar />
-      <br></br>
       <h1>My Mushrooms</h1>
       <div>Search + Filters</div>
       <MushroomList />
