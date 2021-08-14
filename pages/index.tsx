ddd451ad73fbb47cfb61a04caf54c297a04f87f8
Carole Arbogast <arbogast.carole@gmail.com>
2021-08-14 17:44:36 +0300

    feat: add component to create a new mushroom

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
+import React from "react";
+import Modal from "../src/components/Modal";
+import CreateMushroom from "../src/components/Modal/CreateMushroom";
 import MushroomList from "../src/components/MushroomList";
 
 export function Home() {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
   return (
     <>
       <h1>My Mushrooms</h1>
       <div>Search + Filters</div>
+      {isModalOpen && (
+        <Modal onClose={() => setIsModalOpen(false)}>
+          <CreateMushroom />
+        </Modal>
+      )}
+      <button onClick={() => setIsModalOpen(true)}>Add mushroom</button>
       <MushroomList />
     </>
   );
---
+++ src/components/Modal/CreateMushroom.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  edible: string;
+  season: string[];
+  description?: string;
+  image?: string;
+};
+
+export function CreateMushroom() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) =>
+    console.log("submitting mushroom", data);
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input {...register("name", { required: true })}></input>
+
+      <label>Image Link</label>
+      <input {...register("image")}></input>
+
+      <label>Edible/poisonous</label>
+      <select defaultValue="edible" {...register("edible")}>
+        <option value="delicious">Delicious</option>
+        <option value="edible">Edible</option>
+        <option value="poisonous">Poisonous</option>
+      </select>
+
+      <label>Season</label>
+
+      <label>July</label>
+      <input type="checkbox" {...register("season")} value="july"></input>
+
+      <label>August</label>
+      <input type="checkbox" {...register("season")} value="august"></input>
+
+      <label>September</label>
+      <input type="checkbox" {...register("season")} value="june"></input>
+
+      <label>Description</label>
+      <textarea {...register("description")}></textarea>
+
+      {errors.name && <span>This field is required</span>}
+
+      <input type="submit" />
+    </Form>
+  );
+}
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default CreateMushroom;
