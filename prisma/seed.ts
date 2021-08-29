7ae2b5c77862106a47cb596adeaadc3194e49063
Carole Arbogast <arbogast.carole@gmail.com>
2021-08-29 11:25:19 +0300

    feat: add file to seed database wtih fake data

---
+++ prisma/seed.ts
@@ -0,0 +1,37 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const usersData: Prisma.UserCreateManyInput[] = [
+    { username: "Harry Potter" },
+    { username: "Hermione Granger" },
+    { username: "Ron Weasley" },
+  ];
+
+  await prisma.user.createMany({ data: usersData });
+  const users = await prisma.user.findMany({});
+
+  const mushroomsData: Prisma.MushroomCreateManyInput[] = [
+    { name: "Herkkutatti", userId: users[0].id },
+    { name: "Suppilovahvero", userId: users[1].id },
+  ];
+
+  await prisma.mushroom.createMany({ data: mushroomsData });
+  const mushrooms = await prisma.mushroom.findMany({});
+
+  const mushroomDetailsData: Prisma.MushroomDetailsCreateManyInput[] = [
+    { mushroomId: mushrooms[0].id, taste_rating: 3, poison_level: 0 },
+    { mushroomId: mushrooms[1].id, taste_rating: 3, poison_level: 0 },
+  ];
+
+  await prisma.mushroomDetails.createMany({ data: mushroomDetailsData });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
