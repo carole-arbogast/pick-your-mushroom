83365cdf360beaa8fb5eb9137cc1843fb45d27ec
Carole Arbogast <arbogast.carole@gmail.com>
2021-08-30 16:31:51 +0300

    fix: update seeds

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -13,7 +13,12 @@ async function main() {
   const users = await prisma.user.findMany({});
 
   const mushroomsData: Prisma.MushroomCreateManyInput[] = [
-    { name: "Herkkutatti", userId: users[0].id },
+    {
+      name: "Herkkutatti",
+      userId: users[0].id,
+      description: "Rounded cap, green sponge, delicious",
+      image: "/link/to/image",
+    },
     { name: "Suppilovahvero", userId: users[1].id },
   ];
 
@@ -21,11 +26,27 @@ async function main() {
   const mushrooms = await prisma.mushroom.findMany({});
 
   const mushroomDetailsData: Prisma.MushroomDetailsCreateManyInput[] = [
-    { mushroomId: mushrooms[0].id, taste_rating: 3, poison_level: 0 },
-    { mushroomId: mushrooms[1].id, taste_rating: 3, poison_level: 0 },
+    {
+      mushroomId: mushrooms[0].id,
+      taste_rating: 3,
+      poison_level: 0,
+      dyeing: false,
+      ffa_recommended: true,
+      boiling_required: false,
+    },
+    {
+      mushroomId: mushrooms[1].id,
+      taste_rating: 3,
+      poison_level: 0,
+      dyeing: false,
+      ffa_recommended: true,
+      boiling_required: false,
+    },
   ];
 
-  await prisma.mushroomDetails.createMany({ data: mushroomDetailsData });
+  await prisma.mushroomDetails.createMany({
+    data: mushroomDetailsData,
+  });
 }
 
 main()
